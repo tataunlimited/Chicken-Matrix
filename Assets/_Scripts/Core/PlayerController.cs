a053c7e97c43f60ead23396cf92b92734b0a78c9
MSI\taher <taher.samadi@gmail.com>
2026-01-16 04:05:49 -0500

    properly destroy the flash object

--- Assets/_Scripts/Core/PlayerController.cs
+++ Assets/_Scripts/Core/PlayerController.cs
@@ -74,7 +74,7 @@ namespace _Scripts.Core
             Destroy(cloneSprite.GetComponent<Collider2D>());
             var tweenColor = cloneSprite.color;
             tweenColor.a = 0;
-            cloneSprite.DOColor(tweenColor, .5f).SetEase(Ease.Flash).onComplete += () => Destroy(cloneSprite);
+            cloneSprite.DOColor(tweenColor, .5f).SetEase(Ease.Flash).onComplete += () => Destroy(cloneSprite.gameObject);
 
             yield return new WaitForSeconds(detectionInterval);
             detectionCollider.enabled = false;
